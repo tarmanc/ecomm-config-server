f5eb3578b8f039320d74b490975e6fe37e0387da
adyingwish <62962574+adyingwish@users.noreply.github.com>
2021-02-15 23:26:58 +0300

    Initialize Cloud Config Server

---
+++ src/main/java/com/armanc/ecommconfigserver/EcommConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.armanc.ecommconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class EcommConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommConfigServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/armanc/ecommconfigserver/EcommConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.armanc.ecommconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
